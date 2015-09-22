3bf3717427d08be26853688c85ae02d324942d3d
crzmp3 <equaltm@gmail.com>
2015-09-22 20:09:22 +0300

    first

---
+++ weather.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+	http -b http://meteo.by > url.txt
+	grep -n -A 7 '<p class="t ">' url.txt > url_new.txt
+	sed -n 7p url_new.txt
+	sleep $(awk -F "=" '/timeout/ {print $2}' config.ini)
+done
+
